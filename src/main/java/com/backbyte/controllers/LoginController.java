a04966133fcdc538872eb51f084c246951dad072
AlejandroAlonsoGomez <aalonsogomez0@uoc.edu>
2024-11-23 11:54:24 +0100

    actualizado los enrutados de los home y sucursales para users y admins

--- src/main/java/com/backbyte/controllers/LoginController.java
+++ src/main/java/com/backbyte/controllers/LoginController.java
@@ -8,19 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-
     @GetMapping("/login")
     public String login() {
         return "login";  // Muestra la vista del formulario de login
     }
 
-    @GetMapping("/admin/home")
-    public String adminHome() {
-        return "admin/adminHome";  // Redirige a la página de inicio del admin
-    }
-
-    @GetMapping("/user/home")
-    public String userHome() {
-        return "user/userHome";  // Redirige a la página de inicio del usuario
-    }
 }
